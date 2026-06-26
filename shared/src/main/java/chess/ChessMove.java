a6645bca19c16f5bc1ff2298aefa6489e3b48e4b
Luis Rosas <luisr112501@gmail.com>
2026-06-26 13:52:21 -0600

    Filled out ChessMoves.java

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -8,21 +8,28 @@ package chess;
  */
 public class ChessMove {
 
+    private final ChessPosition posStart;
+    private final ChessPosition posEnd;
+    private final ChessPiece.PieceType promotion;
+
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition, ChessPiece.PieceType promotionPiece) {
+        this.posStart = startPosition;
+        this.posEnd = endPosition;
+        this.promotion = promotionPiece;
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return posStart;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return posEnd;
     }
 
     /**
@@ -32,6 +39,6 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return promotion;
     }
 }
