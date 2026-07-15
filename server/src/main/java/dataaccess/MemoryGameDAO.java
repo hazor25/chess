766e32e79ff5d33ce7286bb927bde85f44f54f80
Luis Rosas <luisr112501@gmail.com>
2026-07-15 12:04:37 -0600

    Filled out Memory/GameDAO

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,4 +1,38 @@
 package dataaccess;
 
-public class MemoryGameDAO {
+import model.GameData;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+public class MemoryGameDAO implements GameDAO {
+    private final HashMap<Integer, GameData> games = new HashMap<>();
+
+    @Override
+    public void createGame(GameData game) {
+        games.put(game.gameID(), game);
+    }
+
+    @Override
+    public GameData getGame(int gameID) {
+        return games.get(gameID);
+    }
+
+    @Override
+    public Collection<GameData> listGames() {
+        return games.values();
+    }
+
+    @Override
+    public void updateGame(GameData game) {
+        if (!games.containsKey(game.gameID())) {
+            games.remove(game.gameID());
+        }
+        games.put(game.gameID(), game);
+    }
+
+    @Override
+    public void clear() {
+        games.clear();
+    }
 }
