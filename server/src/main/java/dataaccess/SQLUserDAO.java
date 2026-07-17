6ab802519578d15ee1e17b142ace0866882b1c50
Luis Rosas <luisr112501@gmail.com>
2026-07-17 12:47:34 -0600

    Created SQL DAO files, half filled out UserDAO

---
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -0,0 +1,62 @@
+package dataaccess;
+
+import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
+
+public class SQLUserDAO implements UserDAO {
+
+    public SQLUserDAO() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        configureDatabase();
+    }
+
+    private void configureDatabase() throws DataAccessException {
+
+        String sql = """
+                CREATE TABLE IF NOT EXISTS users (
+                username VARCHAR(255) PRIMARY KEY,
+                password VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL
+            )
+            """;
+
+        try (var conn = DatabaseManager.getConnection();
+             var preparedStatement = conn.prepareStatement(sql)) {
+
+            preparedStatement.executeUpdate();
+
+        } catch (Exception e) {
+            throw new DataAccessException("Unable to configure database", e);
+        }
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        String sql = """
+        INSERT INTO users (username, password, email)
+        VALUES (?, ?, ?)
+        """;
+
+        try (var conn = DatabaseManager.getConnection();
+             var statement = conn.prepareStatement(sql)) {
+            statement.setString(1, user.username());
+            String hashedPassword = BCrypt.hashpw(user.password(), BCrypt.gensalt());
+            statement.setString(2, hashedPassword);
+            statement.setString(3, user.email());
+
+            statement.executeUpdate();
+        }  catch (Exception e) {
+            throw new DataAccessException("Unable to create user", e);
+        }
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
+}
