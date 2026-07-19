ca3794f2dfa35891c88d65a56411578789b0f3bc
Luis Rosas <luisr112501@gmail.com>
2026-07-18 23:51:24 -0600

    Tried to edit test files to see which one was not allowing compiling, read details in autograder saying it was DataaccessException inAPITests and  Server.java, so deleted that and edited server.java to handle Runtime Exception instead

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -8,32 +8,33 @@ public class Server {
 
     private final Javalin javalin;
 
-    public Server() throws DataAccessException {
-        UserDAO userDAO = new SQLUserDAO();
-        AuthDAO authDAO = new SQLAuthDAO();
-        GameDAO gameDAO = new SQLGameDAO();
+    public Server() {
+        try {
+            UserDAO userDAO = new SQLUserDAO();
+            AuthDAO authDAO = new SQLAuthDAO();
+            GameDAO gameDAO = new SQLGameDAO();
 
-        ClearService clearService = new ClearService(userDAO, authDAO, gameDAO);
-        UserService userService = new UserService(userDAO, authDAO);
-        GameService gameService = new GameService(gameDAO, authDAO);
+            ClearService clearService = new ClearService(userDAO, authDAO, gameDAO);
+            UserService userService = new UserService(userDAO, authDAO);
+            GameService gameService = new GameService(gameDAO, authDAO);
 
-        ClearHandler clearHandler = new ClearHandler(clearService);
-        UserHandler userHandler = new UserHandler(userService);
-        GameHandler gameHandler = new GameHandler(gameService);
+            ClearHandler clearHandler = new ClearHandler(clearService);
+            UserHandler userHandler = new UserHandler(userService);
+            GameHandler gameHandler = new GameHandler(gameService);
 
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+            javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
-        javalin.delete("/db", clearHandler::clear);
+            javalin.delete("/db", clearHandler::clear);
+            javalin.post("/user", userHandler::register);
+            javalin.post("/session", userHandler::login);
+            javalin.delete("/session", userHandler::logout);
+            javalin.get("/game", gameHandler::listGames);
+            javalin.post("/game", gameHandler::createGame);
+            javalin.put("/game", gameHandler::joinGame);
 
-        javalin.post("/user", userHandler::register);
-        javalin.post("/session", userHandler::login);
-
-        javalin.delete("/session", userHandler::logout);
-
-        javalin.get("/game", gameHandler::listGames);
-        javalin.post("/game", gameHandler::createGame);
-        javalin.put("/game", gameHandler::joinGame);
-
+        } catch (DataAccessException e) {
+            throw new RuntimeException("Unable to start server", e);
+        }
     }
 
     public int run(int desiredPort) {
